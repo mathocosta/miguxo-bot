196d3fd6bd2f86c62e4cb872b9cbdd3db60ed202
Mario Silva <m_s_r@outlook.com>
2021-10-05 08:27:42 -0300

    Integração com Netlify + Migração TeleBot (#2)
    * autoformat
    
    * adiconando artefatos no ignore
    
    * versão do telegraf que não tem o problema de build
    
    * permitindo any por causa do telegraf
    
    * mudando o import do telegraf pra require
    
    * mais configurações do netlify
    
    * alterando configuração de deploy
    
    * mais mudanças de deploy
    
    * mais scripts de deploy
    
    * corrigindo o import do telegraf
    
    * ajustes de versão do telegraf
    
    * adicionando telebot
    
    * autoformat
    
    * adicionando tipos do telebot
    
    * permitindo any por causa do telebot
    
    * migrando do telegraf pro telebot
    
    * atualizando lockfile
    
    * garantindo que a aplicação não vai crashar caso receibeUpdates falhe
    
    * removendo dependências
    
    * ajustes de dependências

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    mode: 'production', // "production" | "development" | "none"
-    resolve: {
-        extensions: ['*', '.mjs', '.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: 'javascript/auto',
-            }
-        ]
-    }
-}
+  mode: "production", // "production" | "development" | "none"
+  resolve: {
+    extensions: ["*", ".mjs", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+    ],
+  },
+};
